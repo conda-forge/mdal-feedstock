df9f67b9b9896c22dd11fafc7f937716cb5a4079
Paul <runette@gmail.com>
2021-05-30 01:53:04 +0100

    temp fix linux test issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,15 +5,24 @@ set -ex
 mkdir build
 cd build
 
+export TEST=ON
+
+if [ "$(uname)" == "Linux" ]; then
+   export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+   export TEST=OFF
+fi
+
 cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
   -DCMAKE_INCLUDE_PATH=$PREFIX/include \
-  -DENABLE_TESTS=ON \
+  -DENABLE_TESTS=$TEST \
   ..
 
 make -j $CPU_COUNT
 make install
 export PATH=$PATH:$PREFIX/lib
-ctest -VV
+if ["$TEST" == "ON"]; then
+  ctest -VV
+fi
